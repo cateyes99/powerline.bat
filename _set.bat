9e7ea8ea64ba93c19ab72d842e1e1a8aeb71808e
XU Zhaofeng <zhaofeng.xu@iag.co.nz>
2022-05-07 19:29:26 +1200

    Add newline to the prompt.

--- _set.bat
+++ _set.bat
@@ -4,7 +4,7 @@
         set GITBRANCH=
         for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
         if "%GITBRANCH%" == "" (
-            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34m$E[0m$S
+            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34m$_$E[1;31m$g$E[0m$S
         ) else (
-            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$E[0m$S
+            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$_$E[1;31m$g$E[0m$S
         )
