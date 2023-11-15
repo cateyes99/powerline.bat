67391a12305513f09fae7095f7b28444f123dcfc
XU Zhaofeng <xuzf@hotmail.com>
2023-11-15 16:49:12 +1300

    Change the path background colour from blue to cyan.

--- _set.bat
+++ _set.bat
@@ -1,10 +1,23 @@
         @echo off
         for %%a in ("%CD%") do set "PARENT_FOLDER=%%~nxa"
         title %PARENT_FOLDER%
+
         set GITBRANCH=
         for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
-        if "%GITBRANCH%" == "" (
-            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34m$_$E[1;31m$g$E[0m$S
+
+        set ELEVATED=
+        net.exe session 1>NUL 2>NUL && set ELEVATED=1
+
+        if "%ELEVATED%" == "" (
+            if "%GITBRANCH%" == "" (
+                prompt $E[97;46m$S$E[0m$E[97;46m$P$S$E[0m$E[36m$_$E[1;31m$g$E[0m$S
+            ) else (
+                prompt $E[97;46m$S$E[0m$E[97;46m$P$S$E[0m$E[36;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$_$E[1;31m$g$E[0m$S
+            )
         ) else (
-            prompt $E[37;44m$S$E[0m$E[37;44m$P$S$E[0m$E[34;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$_$E[1;31m$g$E[0m$S
+            if "%GITBRANCH%" == "" (
+                prompt $E[97;41m$S$E[0m$E[97;41m$P$S$E[0m$E[31m$_$E[1;36m$g$E[0m$S
+            ) else (
+                prompt $E[97;41m$S$E[0m$E[97;41m$P$S$E[0m$E[31;43m$S$E[0m$E[30;43m%GITBRANCH%$S$E[0m$E[33m$_$E[1;36m$g$E[0m$S
+            )
         )
